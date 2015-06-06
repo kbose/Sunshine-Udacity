b955b7ee8558d5901ed5e37686d00cd28f97b4da
Kevin Wang <kevw@seas.upenn.edu>
2015-06-06 00:29:10 -0500

    Updated to match Udacity's code

--- app/src/main/java/com/example/android/sunshine/app/DetailActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivityFragment.java
@@ -22,110 +22,15 @@ public class DetailActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        // The detail Activity called via intent.  Inspect the intent for forecast data.
         Intent intent = getActivity().getIntent();
-        String forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
-
-        TextView dayView = (TextView) rootView.findViewById(R.id.textDay);
-        TextView dateView = (TextView) rootView.findViewById(R.id.textDate);
-        TextView highView = (TextView) rootView.findViewById(R.id.textHigh);
-        TextView lowView = (TextView) rootView.findViewById(R.id.textLow);
-        TextView weatherView = (TextView) rootView.findViewById(R.id.textWeather);
-
-        dayView.setText(getDay(forecast));
-        dateView.setText(getDate(forecast));
-
-        /*parsing forecast for weather*/
-        String[] forecastSplit = forecast.split("-");
-        weatherView.setText(forecastSplit[1].trim());
-
-        /*parsing for high and low temps*/
-        String[] temps = forecastSplit[2].split("/");
-        highView.setText(temps[0]);
-        lowView.setText(temps[1]);
+        if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+            String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) rootView.findViewById(R.id.detail_text))
+                    .setText(forecastStr);
+        }
 
         return rootView;
     }
 
-    private String getDay(String forecast) {
-        String day = forecast.substring(0, 3);
-
-        switch(day) {
-            case "Mon":
-                day = "Monday";
-                break;
-            case "Tue":
-                day = "Tuesday";
-                break;
-            case "Wed":
-                day = "Wednesday";
-                break;
-            case "Thu":
-                day = "Thursday";
-                break;
-            case "Fri":
-                day = "Friday";
-                break;
-            case "Sat":
-                day = "Saturday";
-                break;
-            case "Sun":
-                day = "Sunday";
-                break;
-            default:
-                day = "error";
-                break;
-        }
-
-        return day;
-    }
-
-    private StringBuffer getDate(String forecast) {
-        String month = forecast.substring(4, 7);
-        StringBuffer date = new StringBuffer();
-
-        switch(month) {
-            case "Jan":
-                date.append("January");
-                break;
-            case "Feb":
-                date.append("February");
-                break;
-            case "Mar":
-                date.append("March");
-                break;
-            case "Apr":
-                date.append("April");
-                break;
-            case "May":
-                date.append("May");
-                break;
-            case "Jun":
-                date.append("June");
-                break;
-            case "Jul":
-                date.append("July");
-                break;
-            case "Aug":
-                date.append("August");
-                break;
-            case "Sep":
-                date.append("September");
-                break;
-            case "Oct":
-                date.append("October");
-                break;
-            case "Nov":
-                date.append("November");
-                break;
-            case "Dec":
-                date.append("December");
-                break;
-            default:
-                return null;
-        }
-
-        date.append(" ");
-        date.append(forecast.substring(8, 10));
-        return date;
-    }
 }
